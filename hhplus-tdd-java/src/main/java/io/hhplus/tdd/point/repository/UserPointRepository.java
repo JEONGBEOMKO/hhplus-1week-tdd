87f0f1e9f42c6d6d3176e28d1f0f7d69310090b7
JEONGBEOM KO <beom9807@gmail.com>
2024-09-26 23:00:41 +0900

    #1 - 사용자 메서드 작성
    포인트 충전, 조회 메서드 작성

--- hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/repository/UserPointRepository.java
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/repository/UserPointRepository.java
@@ -1,9 +1,9 @@
 package io.hhplus.tdd.point.repository;
 
 
-import io.hhplus.tdd.point.aggregate.entity.UserPoint;
+import io.hhplus.tdd.point.domain.model.entity.UserPoint;
 
 public interface UserPointRepository {
-    UserPoint save(long userId, long amount);
+    UserPoint save(UserPoint userPoint);
     UserPoint findById(long userId);
 }
--- hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/repository/UserPointRepositoryImpl.java
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/repository/UserPointRepositoryImpl.java
@@ -1,9 +1,11 @@
 package io.hhplus.tdd.point.repository;
 
 import io.hhplus.tdd.database.UserPointTable;
-import io.hhplus.tdd.point.aggregate.entity.UserPoint;
+import io.hhplus.tdd.point.domain.model.entity.UserPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Objects;
 
 @Repository
 @RequiredArgsConstructor
@@ -11,12 +13,16 @@ public class UserPointRepositoryImpl implements UserPointRepository{
     //
     private final UserPointTable userPointTable;
 
+    /*사용자 포인트 저장*/
     @Override
-    public UserPoint save(long userId, long amount) {
-        //
-        return this.userPointTable.insertOrUpdate(userId, amount);
+    public UserPoint save(UserPoint userPoint) {
+        if(Objects.isNull(userPoint)){
+            throw new IllegalArgumentException("사용자가 없습니다.");
+        }
+        return userPointTable.insertOrUpdate(userPoint.id(), userPoint.point());
     }
 
+    /* 사용자 포인트 조회 */
     @Override
     public UserPoint findById(long userId) {
         //
